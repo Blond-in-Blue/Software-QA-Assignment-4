a007a161aee014cec59918fc6f02276788b5eb47
johnyboy63 <jd_stewart_6312@hotmail.com>
2016-04-22 21:26:34 -0500

    Started on retirement

--- src/Retirement.java
+++ src/Retirement.java
@@ -1,5 +1,34 @@
+import java.util.Scanner;
+
 /**
  * Created by Kemp on 4/19/2016.
  */
 public class Retirement {
+
+    public static void main(String[] args) {
+
+        int currentAge;
+        double annualSalary;
+        double percentageSaved;
+        double savingsGoal;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter your current age: ");
+        currentAge = input.nextInt();
+
+        System.out.println("Enter your current annual salary: ");
+        annualSalary = input.nextDouble();
+
+        System.out.println("Enter the percentage of your annual salary that is saved for retirement: ");
+        percentageSaved = input.nextDouble();
+        percentageSaved = percentageSaved *2;
+
+        System.out.println("What is your retirement savings goal? ");
+        savingsGoal = input.nextDouble();
+
+        
+
+    }
+
 }
